1dffcd9d7f0c7137a5beef8a1e5b4c5308871690
MarceauBardey <marceau.bardey@gmail.com>
2018-11-20 16:52:09 +0100

    1ère modification du programme

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -1,12 +1,8 @@
 #!/bin/bashm
 cd ~
-date=$( date '+%d %B %Y')
-username=$(whoami)
 
-echo "Bienvenu $username, nous sommes le $date"
-
-read -p "Quel repertoire vous intersse aujourd'hui ?" repertoire
-find -name $repertoire -exec ls {} \;
+read -p "Veuillez indiquer le répertoire pour lequel vous souhaitez voir tous les changements éffectués depuis une semaine " repertoire
+find -name $repertoire -exec find -type f -mtime -7 {} \;
 
 cd ~/gitstuff/MoSEF-projet-2018
 
