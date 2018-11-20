99e0794bdbaefa1db25b07b9ba4f8e863c48b782
MarceauBardey <marceau.bardey@gmail.com>
2018-11-20 17:39:56 +0100

    1ère modification du programe

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -1,5 +1,12 @@
 #!/bin/bashm
 cd ~
+date=$(date +%d %B %Y)
+username=$(whoami)
+
+echo "Bienvenue $username, nous sommes le $date"
+
+read -p "Quel répertoire vous intéresse aujourd'hui ?" rep
+find -name $rep -exec ls {} \;
 
 read -p "Veuillez indiquer le répertoire pour lequel vous souhaitez voir tous les changements éffectués depuis une semaine " repertoire
 find -name $repertoire -exec find -type f -mtime -7 {} \;
