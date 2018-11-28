14c1d7b71b449bf34dab1f92667667229dccf8d3
MarceauBardey <marceau.bardey@gmail.com>
2018-11-28 08:54:13 +0100

    Mise à jour du programme

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -8,10 +8,7 @@ echo "Bienvenue $username, nous sommes le $date"
 read -p "Quel répertoire vous intéresse aujourd'hui ?" rep
 find -name $rep -exec ls {} \;
 
-read -p "Veuillez indiquer le répertoire pour lequel vous souhaitez voir tous les changements éffectués depuis une semaine " repertoire
-find -name $repertoire -exec find -type f -mtime -7 {} \;
-
-cd ~/gitstuff/MoSEF-projet-2018
+cd ~
 
 
 
