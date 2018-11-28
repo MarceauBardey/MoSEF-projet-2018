cd80b5019e812973e05ee0bbd2b09ed2546fdf0f
MarceauBardey <marceau.bardey@gmail.com>
2018-11-28 09:07:05 +0100

    Modification du programme projet 2018

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -1,9 +1,8 @@
 #!/bin/bashm
 cd ~
 date=$(date +%d %B %Y)
-username=$(whoami)
 
-echo "Bienvenue $username, nous sommes le $date"
+echo "Bienvenue $USER, nous sommes le $date"
 
 read -p "Quel répertoire vous intéresse aujourd'hui ?" rep
 find -name $rep -exec ls {} \;
