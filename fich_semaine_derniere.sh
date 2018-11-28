caee0217d64409ab0d71436ed2bbb3f283bb355b
MarceauBardey <marceau.bardey@gmail.com>
2018-11-28 09:25:16 +0100

    Modification Programme

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -1,11 +1,12 @@
-#!/bin/bashm
+#!/bin/bash
 cd ~
 date=$(date +%d %B %Y)
 
 echo "Bienvenue $USER, nous sommes le $date"
 
-read -p "Quel répertoire vous intéresse aujourd'hui ?" rep
-find -name $rep -exec ls {} \;
+read -p "Quel répertoire vous intéresse aujourd'hui (Veuillez indiquer le chemin complet depuis ~) ?" rep
+cd $rep
+ls
 
 cd ~
 
